4f66e07a91829c262c518eaafca90fbe75cc28cb
jacheampong <jacheampong@gmail.com>
2020-04-08 20:54:59 -0400

    #5: Add paging support to finder method

--- src/main/java/com/ja/dev/eventapi/repos/EventRepository.java
+++ src/main/java/com/ja/dev/eventapi/repos/EventRepository.java
@@ -1,14 +1,15 @@
 package com.ja.dev.eventapi.repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.ja.dev.eventapi.entities.Event;
 import java.lang.String;
-import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 	
-	List<Event> findByName(@Param("name") String name);
+	Page<Event> findByName(@Param("name") String name, Pageable pageable);
 
 }
