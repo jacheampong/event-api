c2d7745b955c0fc875292c08ed767282afa3420b
jacheampong <jacheampong@gmail.com>
2020-04-08 20:43:38 -0400

    #4: adding findByName method

--- src/main/java/com/ja/dev/eventapi/repos/EventRepository.java
+++ src/main/java/com/ja/dev/eventapi/repos/EventRepository.java
@@ -1,9 +1,14 @@
 package com.ja.dev.eventapi.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ja.dev.eventapi.entities.Event;
+import java.lang.String;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+	
+	List<Event> findByName(@Param("name") String name);
 
 }
